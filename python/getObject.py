094efe79c674c4b10d5681772def8487f396bbd9
dwk <71767816+dwkim922@users.noreply.github.com>
2022-11-07 09:21:03 +0900

    Update getObject.py

--- python/getObject.py
+++ python/getObject.py
@@ -5,7 +5,7 @@ session = boto3.session.Session(profile_name='zadara')
 s3_client = session.client(
     service_name='s3',
     region_name='us-east-1',
-    endpoint_url='https://vsa-00000004-kt-g-object-01.zadarazios.com',
+    endpoint_url='',
 )
 
 objectName = 'uploadingViaPHP.txt'
