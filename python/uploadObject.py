6529f63390dd00f1961994572b5846d3218cf979
dwk <71767816+dwkim922@users.noreply.github.com>
2023-03-13 11:17:03 +0900

    Update uploadObject.py
    added multipart config

--- python/uploadObject.py
+++ python/uploadObject.py
@@ -8,10 +8,17 @@ s3_client = session.client(
     endpoint_url='',
 )
 
+# Uncomment if you want to set multipart
+#config = TransferConfig(multipart_threshold=1024 * 25, #if 25MB or above, will initiate multipart
+#                        max_concurrency=20,
+#                        multipart_chunksize=1024 * 10, #segment will be 10MB
+#                        use_threads=True)
+
 objectName = 'test.txt'
 bucket = 'tempurl'
 
 response = s3_client.upload_file(
     objectName, bucket, objectName,
     ExtraArgs={'ContentType': 'abc/test'}
+#    Config=config #Uncomment for multipart config 
      )
